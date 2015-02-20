71b65d88ab72caecc9a83dba066e59bbac4373d4
cjohnston <cjohnston@redcap.brc.iop.kcl.ac.uk>
2015-02-20 14:02:56 +0000

    fixed issues with attaching autocomplete to object

--- hooks.php
+++ hooks.php
@@ -1,12 +1,48 @@
 <?php
 
 
+/***************
+*
+* Dictionary Plugin
+* 
+****************/
+
+/* Add in a link to the Dictionary plugin to the control centre*/
+function redcap_control_center()
+{
+
+    print  "<div id='ac_cc_link' style='clear:both;margin:0 -6px 3px;border-top:1px solid #ddd;'>
+              <b style='padding:5px'>Dictionaries Plugin</b>
+                <div style='padding-top:5px;'></div>
+                <span style='padding-left:10px'>
+                  <a href='/plugins/dictionaries/'>Dictionaries</a><br/>
+                </span>
+            </div>";
+
+    // Use JavaScript/jQuery to append our link to the bottom of the left-hand menu
+    print  "<script type='text/javascript'>
+            $(document).ready(function(){
+                // Append link to left-hand menu
+                $( 'div#ac_cc_link' ).appendTo( 'div#control_center_menu' );
+            });
+            </script>";
+
+}
+
+
+/***************
+*
+* Autocomplete Plugin
+* 
+****************/
+
+
 /* Modify data entry forms to add javascript for any registered autocomplete fields */
 
 function autocomplete($field_name, $dict_name){
   $source = APP_PATH_WEBROOT_FULL.'plugins/autocomplete/autocomplete.php?dictionary='.$dict_name;
   echo "<script type='text/javascript'>
-          add_autocomplete('$field_name', '$dict_name', '$source');
+          $(function(){add_autocomplete('$field_name', '$dict_name', '$source'); });
         </script>";
 
 }
